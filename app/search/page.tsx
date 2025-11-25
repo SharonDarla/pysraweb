c3edc81edbe593232bc49c24799fb1d2b0275008
amkhrjee <aniruddha.mukherji@outlook.com>
2025-11-26 02:00:02 +0530

    add: tanstack query instead of useEffect

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,56 +3,73 @@ import ResultCard from "@/components/result-card";
 import SearchBar from "@/components/search-bar";
 import { SearchResults } from "@/utils/types";
 import { Flex, Spinner, Text } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+
+const getSearchResults = async (
+  query: string | null
+): Promise<SearchResults | null> => {
+  if (!query) return null;
+
+  const BASE_URL = "http://10.195.102.16:8000";
+
+  const res = await fetch(`${BASE_URL}/search?q=${encodeURIComponent(query)}`);
+  if (!res.ok) {
+    throw new Error("Network Error");
+  }
+  return res.json();
+};
 
 export default function SearchPage() {
   const searchParams = useSearchParams();
-  const [query, setQuery] = useState(searchParams.get("q"));
-  const [searchResults, setSearchResults] = useState<SearchResults>();
-  const [isError, setIsError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [isSearchMade, setIsSearchMade] = useState(false);
+  const query = searchParams.get("q");
 
-  useEffect(() => {
-    const fetchResults = () => {
-      // TODO: Use tanstack query
-      if (query && !isSearchMade) {
-        // fetch(`http://10.195.102.16:8000/search?q=${encodeURIComponent(query)}`)
-        //   .then((res) => res.json())
-        //   .then((data) => {
-        //     console.log(data);
-
-        //     if (data.error) {
-        //       setIsError(true);
-        //       setErrorMessage(data.error);
-        //     } else {
-        //       setSearchResults(data);
-        //     }
-        //   });
-
-        console.log(query);
-        setIsSearchMade(true);
-      }
-    };
-
-    fetchResults();
-  }, []);
+  const {
+    data: searchResults,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["search", query],
+    queryFn: ({ queryKey }) => getSearchResults(queryKey[1] as string | null),
+    enabled: !!query,
+  });
 
   return (
     <>
       {/* Navbar and search */}
       <SearchBar searchQuery={query} />
+
       {/* Search results and filters */}
       <Flex
-        gap={"4"}
+        gap="4"
         p="3"
         style={{ marginLeft: "8.2rem", width: "65%" }}
-        direction={"column"}
+        direction="column"
       >
-        {/* <ResultCard experimentTitle="Ampliseq of SARS-CoV-2" />
-        <ResultCard experimentTitle="Severe acute respiratory syndrome coronavirus 2" /> */}
-        {searchResults ? (
+        {!query ? (
+          <Flex align="center" justify="center">
+            <Text>Start by typing a search query above.</Text>
+          </Flex>
+        ) : isLoading ? (
+          <Flex
+            gap="2"
+            align="center"
+            style={{ width: "65%" }}
+            justify="center"
+          >
+            <Spinner size="3" />
+            <Text>Search in progress</Text>
+          </Flex>
+        ) : isError ? (
+          <Flex gap="2" align="center" justify="center">
+            <Text>
+              {error instanceof Error
+                ? error.message
+                : "An error occurred while searching"}
+            </Text>
+          </Flex>
+        ) : searchResults ? (
           Object.entries(searchResults).flatMap(([studyAcc, results]) => (
             <ResultCard
               key={studyAcc}
@@ -61,21 +78,9 @@ export default function SearchPage() {
               experimentTitle={results[0].experiment_title}
             />
           ))
-        ) : isError ? (
-          <Flex gap={"2"} align={"center"} justify={"center"}>
-            <Text color="red">
-              {errorMessage || "An error occurred while searching"}
-            </Text>
-          </Flex>
         ) : (
-          <Flex
-            gap={"2"}
-            align={"center"}
-            style={{ width: "65%" }}
-            justify={"center"}
-          >
-            <Spinner size={"3"} />
-            <Text>Search in progress</Text>
+          <Flex align="center" justify="center">
+            <Text>No results found.</Text>
           </Flex>
         )}
       </Flex>
