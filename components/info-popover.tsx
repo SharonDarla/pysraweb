c9689aadf735aa8b4843e00b595fe345abc4b3cc
amkhrjee <aniruddha.mukherji@outlook.com>
2025-11-27 03:47:33 +0530

    add: summary field

---
+++ components/info-popover.tsx
@@ -0,0 +1,17 @@
+import { InfoCircledIcon } from "@radix-ui/react-icons";
+import { Link, Popover, Text } from "@radix-ui/themes";
+
+export default function InfoPopOver({ infoText }: { infoText: string }) {
+  return (
+    <Popover.Root>
+      <Popover.Trigger>
+        <InfoCircledIcon style={{ paddingRight: "0.75rem" }} />
+      </Popover.Trigger>
+      <Popover.Content width="360px">
+        <Text size="2">
+          {infoText}. Read <Link href="#">our paper</Link> to learn more.
+        </Text>
+      </Popover.Content>
+    </Popover.Root>
+  );
+}
