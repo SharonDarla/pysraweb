e4b2c30d7dc1da605f56d029029741fd70a21347
amkhrjee <aniruddha.mukherji@outlook.com>
2025-12-20 13:47:48 +0530

    fix: navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,10 @@
-"use client";
 import { Box, Flex, Link } from "@radix-ui/themes";
 import GitHubButton from "./github-button";
 import ThemeToggle from "./theme-toggle";
 
 export default function Navabar() {
   return (
-    <Flex style={{ zIndex: 10 }} justify="between" align="center" p="3">
+    <Flex justify="between" align="center" p="3">
       <Flex gap={"4"} align={"center"}>
         <Link href="https://saket-choudhary.me/pysradb/index.html">Docs</Link>
         <Link href="#">Paper</Link>
