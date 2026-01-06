9b1feef839179a7c997917df719640f3eae7a0db
amkhrjee <aniruddha.mukherji@outlook.com>
2026-01-07 01:08:11 +0530

    fix: meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "pysradb search",
-  description: "search sequence data through sra, geo and ena",
+  title: "pysradb",
+  description: "Discover GEO & SRA datasets",
 };
 
 export default function RootLayout({
